2a25f2ee7d23d3153721456dfa0411319f2fe692
rfshawness <rfshawness@gmail.com>
2024-06-17 15:09:15 -0700

    Notes added to .sh file specifics

--- basic/basic.sh
+++ basic/basic.sh
@@ -1,3 +1,4 @@
+# changed argument specifications
 for file in "../datapoints/$1"*.txt
 do
    python3 basic_3.py "$file" "$2".txt
--- memory_efficent/efficient.sh
+++ memory_efficent/efficient.sh
@@ -1,3 +1,4 @@
+# specify the path to the input file and output file
 for i in {1..15}
 do
    python3 efficient_3.py "$1" "$2"
